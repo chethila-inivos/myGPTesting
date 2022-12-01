13ba1abcaa421cf1c25c07a7319a14654ed787e8
Chethila Mallawaarachchi <Chethila.Mallawaarachchi@inivossl.com>
2022-12-01 09:57:04 +0530

    Initial commit

---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,26 @@
+import factory.DriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import pageEvents.HtmlElementEvents;
+import utils.BaseTest;
+
+public class TestSuite extends BaseTest {
+
+    public static WebDriver driver;
+
+//    @Test
+//    public void openWebPage() throws InterruptedException {
+//        driver = DriverFactory.getDriver("chrome");
+//        driver.get("http://localhost:4200/#/");
+//        Thread.sleep(10000);
+//    }
+
+    @Test
+    public void clickButton() throws InterruptedException {
+        driver = DriverFactory.getDriver("chrome");
+        driver.get("http://localhost:4200/#/");
+        HtmlElementEvents htmlElementEvents = new HtmlElementEvents();
+        Thread.sleep(3000);
+        htmlElementEvents.clickButton("//*[@id=\"dropdownMenuButton1\"]/span[1]");
+    }
+}
